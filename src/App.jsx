f992e66f04a9aa88d17102de84ee15b187ad484b
Sakshishrma123 <sakshisharma.dsvv@gmail.com>
2024-02-07 16:14:23 +0530

    set up the project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-primary m-5 pt-5'>Welcome To PreOrder Shop</h1>
+    </>
+  )
+}
+
+export default App
