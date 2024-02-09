8e5d45ed8779a01d333c8c0fcb07a946ecedc0a1
Sakshishrma123 <sakshisharma.dsvv@gmail.com>
2024-02-10 01:01:03 +0530

    complete the design of home page

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import productData from "../products.json";
+import { Link } from "react-router-dom";
+import SelectedCategory from "../components/SelectedCategory";
 const title = (
   <h2>
     Search Your One From <span>Thousands</span> of Products
@@ -23,16 +25,17 @@ const bannerList = [
 const desc = "We have the largest collection of products";
 const Banner = () => {
   const [searchInput, setSearchInput] = useState("");
-  const [filteredProdcuts, setfilteredProducts] = useState(productData);
+  const [filteredProducts, setfilteredProducts] = useState(productData);
   
   // search functionalty
 
   const handleSearch = e => {
-    console.log(e.target.value);
     const searchTerm = e.target.value;
     setSearchInput(searchTerm);
 
-    // filtering products based don search
+    // filtering products based on search
+    const filtered = productData.filter((product) => product.name.toLowerCase().includes(searchTerm, toLowerCase()));
+    setfilteredProducts(filtered);
    
   }
 
@@ -42,6 +45,7 @@ const Banner = () => {
         <div className="banner-content">
           {title}
           <form>
+            <SelectedCategory select={"all"} />
             <input
               type="text"
               name="search"
@@ -49,7 +53,20 @@ const Banner = () => {
               placeholder="Search your product"
               value={searchInput} onChange={handleSearch}
             />
+            <button type="submit">
+              <i className="icofont-search"></i>
+            </button>
           </form>
+          <p>{desc}</p>
+          <ul className="lab-ul">
+            {
+              searchInput && filteredProducts.map((product, i) => 
+              <li key={i}>
+                <Link to={`/shop/${product.id}`}>{product.name}</Link>
+              </li>
+              )
+            }
+          </ul>
         </div>
       </div>
     </div>
